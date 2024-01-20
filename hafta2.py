0902901743fa5cff80d4d5654e6c02fe40a1cbde
Sefacls <156893554+Sefacls@users.noreply.github.com>
2024-01-20 05:51:36 -0800

    Create hafta2.py

---
+++ hafta2.py
@@ -0,0 +1,21 @@
+isim="ahmet"
+
+print("merhaba," + isim + "hoşgeldin")
+
+sayi = -7
+print("mutlak değer",abs(sayi))
+
+us = pow(2,3)
+print("sayi ussu",us)   
+
+from math import sqrt
+sayi = 25
+karekok= sqrt(sayi)
+print("sayinin karekökü",karekok)
+
+ondalik=10.25
+yukari=ceil(ondalik)
+asaği=floor(ondalik)
+print("yukari",yukari)
+print("asagi",asagi)
+ 
