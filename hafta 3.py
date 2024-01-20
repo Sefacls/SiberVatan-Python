fd4677ea5b3934bebee97f136964b94c769b8a08
Sefacls <156893554+Sefacls@users.noreply.github.com>
2024-01-20 05:50:54 -0800

    Create hafta 3.py

---
+++ hafta 3.py
@@ -0,0 +1,34 @@
+sayi1=42
+sayi2=-17
+ondalik4 = 8.5
+print(type(sayi1))
+
+sayi3=1.5
+sayi4 = -5.5
+print(type(sayi4))
+
+dogru_mu=True
+yanlis_mi=False
+print(type(dogru_mu))
+
+sayi_float = float(sayi1)
+print("int to float", sayi_float)
+
+sayi_int = int(sayi4)
+print("float to int",sayi_int)
+sayi_bool = int(dogru_mu)
+print("bool to int", sayi_bool)
+
+sayi_int = int(dogru_mu)
+print("float to int",sayi_bool)
+
+kullanici_ad = input("adinizi giriniz:")
+print("Hosgeldin ",kullanici_ad)
+
+sayi5 = int(input("sayi gir:"))
+sayi6 = int(input("sayi gir:"))
+print(sayi5+sayi6)
+
+sayi7 = input("1.sayiyi gir")
+sayi8 = input("2.sayiyi gir")
+print(int(sayi7)+int(sayi8))
