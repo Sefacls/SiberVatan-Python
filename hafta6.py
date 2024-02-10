c6eb4d6f62a1ff5016171cb68cd764bf9100f6e6
Sefacls <156893554+Sefacls@users.noreply.github.com>
2024-02-09 22:41:09 -0800

    Create hafta6.py

---
+++ hafta6.py
@@ -0,0 +1,35 @@
+meyveler=["elma","kivi","limon","böğürtlen"]
+
+for meyve in meyveler:
+     print(meyve)
+print(meyveler[3])
+sayilar=[10, 20 , 30 , 40 ]
+
+toplam = 0
+
+for sayı in sayilar:
+    toplam=sayi
+ print(toplam)   
+
+sayilar= [1,2,3,4,5,6,77,88]
+for sayi in sayilar:
+  if sayi %2==1 :
+    continue
+  else:
+    print(sayi)
+    print("siberVatan")
+
+sayi=int(input("bir sayiı gir:"))
+sayac=0
+while sayac<=sayi:
+ print(sayac)
+ sayac+=1
+
+
+list=[]
+sayac=0
+while sayac <5:
+sayilar=int(input("bir sayı gir:"))
+list.append(sayilar)
+1+=1
+print(list)
